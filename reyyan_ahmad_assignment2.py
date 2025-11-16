62afda83df1a738f1d7d819a607c6a90203443e7
reyyanahmad88-sketch <reyyanahmad88@gmail.com>
2025-11-16 15:49:16 +0100

    Add python file

---
+++ reyyan_ahmad_assignment2.py
@@ -0,0 +1,9 @@
+# Start der OOP Aufgabe – wird später erweitert
+
+class Haustier:
+    def __init__(self, name, tierart):
+        self.name = name
+        self.tierart = tierart
+        self.hunger = 50
+        self.glueck = 50
+        self.energie = 50
